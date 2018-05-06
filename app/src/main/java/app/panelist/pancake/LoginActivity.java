867875a1687c1260786114fbec8ccf324a6030bf
RaBuko <raf.buko@gmail.com>
2018-05-06 18:50:07 +0100

    Done LoginActivity view

--- app/src/main/java/app/panelist/pancake/LoginActivity.java
+++ app/src/main/java/app/panelist/pancake/LoginActivity.java
@@ -2,12 +2,24 @@ package app.panelist.pancake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class LoginActivity extends AppCompatActivity {
-	
+public class LoginActivity extends AppCompatActivity
+{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		//ImageView goView = findViewById(R.id.goView);
+		/*
+		goView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				goView.setVisibility(View.INVISIBLE);
+				// gdy nacisnie sie przycisk dalej - do nastepnego activity
+			}
+		});*/
 	}
 }
