9ca5602553d5acd132b8cef4248493cdfc00c472
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-05-06 18:13:42 +0200

    Make hello world clicky OwO

--- app/src/main/java/app/panelist/pancake/HelloWorldActivity.java
+++ app/src/main/java/app/panelist/pancake/HelloWorldActivity.java
@@ -2,6 +2,8 @@ package app.panelist.pancake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class HelloWorldActivity extends AppCompatActivity {
@@ -12,5 +14,13 @@ public class HelloWorldActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_helloworld);
 		
 		((TextView)findViewById(R.id.helloWorldTextView)).setText(R.string.helloworld);
+	}
+	
+	public void onClick(View view) {
+		if (view == findViewById(R.id.helloWorldTextView)) {
+			findViewById(R.id.easterEggImageView).setVisibility(View.VISIBLE);
+		} else if (view == findViewById(R.id.easterEggImageView)) {
+			findViewById(R.id.easterEggImageView).setVisibility(View.INVISIBLE);
+		}
 	}
 }
