7240ea850cf08ffd578e9ba51defcb5fce649d8f
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-05-06 16:48:58 +0200

    Print hello world!

--- app/src/main/java/app/panelist/pancake/HelloWorldActivity.java
+++ app/src/main/java/app/panelist/pancake/HelloWorldActivity.java
@@ -2,12 +2,15 @@ package app.panelist.pancake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class HelloWorldActivity extends AppCompatActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
+		setContentView(R.layout.activity_helloworld);
+		
+		((TextView)findViewById(R.id.textView)).setText("Hello World!");
 	}
 }
