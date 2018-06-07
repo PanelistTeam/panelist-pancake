82557200735bd83755db91ee0024000572cd25bd
Damian Korzekwa <damian.korzekwa@gmail.com>
2018-06-07 18:10:05 +0200

    Provide Toolbar in CreateAskroomActivity with back button and title

--- app/src/main/java/app/panelist/pancake/CreateAskroomActivity.java
+++ app/src/main/java/app/panelist/pancake/CreateAskroomActivity.java
@@ -2,8 +2,7 @@ package app.panelist.pancake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import java.util.Objects;
+import android.support.v7.widget.Toolbar;
 
 public class CreateAskroomActivity extends AppCompatActivity {
 	
@@ -12,9 +11,16 @@ public class CreateAskroomActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_askroom);
 		
-		android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+		Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
-		Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(true);
-		getSupportActionBar().setIcon(R.drawable.ic_arrow_back_black_24dp);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setDisplayShowHomeEnabled(true);
+		setTitle(R.string.title_createAskroomActivity);
+	}
+	
+	@Override
+	public boolean onSupportNavigateUp() {
+		onBackPressed();
+		return true;
 	}
 }
