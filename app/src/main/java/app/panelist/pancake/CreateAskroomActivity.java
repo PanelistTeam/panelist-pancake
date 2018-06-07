d50ce65679052a589cea9557999832bdb2f00d26
RaBuko <raf.buko@gmail.com>
2018-06-07 13:59:00 +0100

    CreateAskroom view done, toolbar fixes

--- app/src/main/java/app/panelist/pancake/CreateAskroomActivity.java
+++ app/src/main/java/app/panelist/pancake/CreateAskroomActivity.java
@@ -3,11 +3,20 @@ package app.panelist.pancake;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class CreateAskroomActivity extends AppCompatActivity {
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_askroom);
+		
+		android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(true);
+		getSupportActionBar().setIcon(R.drawable.ic_arrow_back_black_24dp);
 	}
+	
 }
